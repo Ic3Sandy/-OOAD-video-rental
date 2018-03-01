6c7769b5eee8df499767084f79a26a40397c79c2
Ic3Sandy <monkey_king2@hotmail.com>
2018-03-01 17:25:39 +0700

    Initial commit this dev

--- Main.java
+++ Main.java
@@ -5,16 +5,15 @@ class Main{
         Store liew = new Store();
         Customer customer = new Customer();
         
-            while(true)
+            while(true){
                 list_custom = customer.getAbleRent(liew);
                 amount_vdo = liew.countListVdo();
                 ran_customer = rand.nextInt(list_custom.length) + 1;
-                ran_vdo = liew.getVdo(rand.nextInt(amount_vdo) + 1;)
+                ran_vdo = liew.getVdo(rand.nextInt(amount_vdo) + 1);
 
-                if (!liew.rent(list_custom[ran_customer], ran_vdo);)       
+                if (!liew.rent(list_custom[ran_customer], ran_vdo))       
                     break;            
-            
+            }
         }
 
     }
-}
