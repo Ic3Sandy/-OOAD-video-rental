2b04c503d71aa36c544256f4ffab864474891cb7
kcisss <god_jjay@hotmail.com>
2018-03-02 17:19:11 +0700

    dev_j(1)

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ class Main{
         
 
             while(!((customer.getAbleRent(liew)).length == 0 && liew.countListVdo() == 0)){
-                list_customer = customer.getAbleRent(liew));
+                list_customer = customer.getAbleRent(liew);
                 ran_customer = rand.nextInt(list_customer.length) + 1; 
 
                 liew.rent(list_custom[ran_customer]);
